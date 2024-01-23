01179c9cc003de21f8e79fea247afa378f6827dd
João Victor Ferreira Borba <60452036+J-Borba@users.noreply.github.com>
2024-01-22 23:56:48 -0300

    Curso-02-FilmesApi Get and Post

---
+++ curso-02-FilmesAPI/curso-02-FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,26 @@
+﻿using curso_02_FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace curso_02_FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        [HttpGet]
+        [Route("/buscar")]
+        public List<Filme> GetFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpPost]
+        [Route("/adicionar")]
+        public void AdicionarFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+        }
+    }
+}
---
+++ curso-02-FilmesAPI/curso-02-FilmesAPI/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace curso_02_FilmesAPI.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O título do filme é obrigatório.")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O genero do filme é obrigatório.")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode ser maior que 50 caracteres.")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A duração do filme é obrigatória.")]
+    [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos.")]
+    public int Duracao { get; set; }
+}
