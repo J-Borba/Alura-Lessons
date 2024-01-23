c7a67b12efa968a6cf322bb1a6818e080ae939ea
João Victor Ferreira Borba <60452036+J-Borba@users.noreply.github.com>
2024-01-23 20:14:56 -0300

    API Get and Post Update

--- curso-02-FilmesAPI/curso-02-FilmesAPI/Controllers/FilmeController.cs
+++ curso-02-FilmesAPI/curso-02-FilmesAPI/Controllers/FilmeController.cs
@@ -1,6 +1,5 @@
 ﻿using curso_02_FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.ComponentModel.DataAnnotations;
 
 namespace curso_02_FilmesAPI.Controllers
 {
@@ -9,18 +8,28 @@ namespace curso_02_FilmesAPI.Controllers
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
-        [HttpGet]
-        [Route("/buscar")]
-        public List<Filme> GetFilmes()
+        private static int Id = 0;
+
+        [Route("/[controller]/buscar"), HttpGet]
+        public IEnumerable<Filme> GetFilmes()
         {
             return filmes;
         }
 
-        [HttpPost]
-        [Route("/adicionar")]
-        public void AdicionarFilme([FromBody] Filme filme)
+        [Route("/[controller]/buscar/{id}"), HttpGet]
+        public IActionResult GetFilmeById(int id)
         {
+            var result = filmes.FirstOrDefault(f => f.Id == id);
+
+            return result == null ? NotFound() : Ok(result);
+        }
+
+        [Route("/[controller]/adicionar"), HttpPost]
+        public IActionResult AdicionarFilme([FromBody] Filme filme)
+        {
+            filme.Id = Id++;
             filmes.Add(filme);
+            return CreatedAtAction(nameof(GetFilmeById), new { id = filme.Id }, filme);
         }
     }
 }
